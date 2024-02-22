7aa2ea478b0cb11f48761e8b5c5d055f866bf03d
João Afonso <joaoafonso386@gmail.com>
2024-02-22 01:12:44 +0000

    Init bash script for CI

---
+++ kubernetes/scripts/ci/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo -e "You need to specify a resource as argument\nUsage: $0 <file.yaml> \nUsage: $0 <dir/> "
+    exit 1
+fi
+
+
+sh ./scripts/add_namespace.sh
+
+kubectl apply -f $1 -n fibo
