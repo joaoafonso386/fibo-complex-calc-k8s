ef9801823039d5f56f9551e98f43e548b9539abb
João Afonso <joaoafonso386@gmail.com>
2024-02-23 00:25:53 +0000

    Update CI bash script

--- kubernetes/scripts/ci/install.sh
+++ kubernetes/scripts/ci/install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+set -e
+
+SCRIPT_NAME=$0
+RESOURCES=$#
+RESOURCE_NAME=$@
 
 if [ $# -eq 0 ]; then
-    echo -e "You need to specify a resource as argument\nUsage: $0 <file.yaml> \nUsage: $0 <dir/> "
+    echo "
+
+    You need to specify a resource as argument
+    Usage: $SCRIPT_NAME <file.yaml>
+    Usage: $SCRIPT_NAME <dir/>
+
+    "
     exit 1
 fi
 
 sh ./scripts/add_namespace.sh
 
-echo -e "\e[1;31mApplying $# k8s resources: $@\e[0m"
+echo -e "\e[1;31mApplying $RESOURCES k8s resources: $RESOURCE_NAME\e[0m"
 
 for resource in "$@"; do
     kubectl apply -f "$resource" -n fibo
