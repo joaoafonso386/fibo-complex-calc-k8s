be9fe57026055919df2601a7999a8866016553f1
João Afonso <joaoafonso386@gmail.com>
2024-02-25 23:10:00 +0000

    Update namespace script

--- kubernetes/scripts/add_namespace.sh
+++ kubernetes/scripts/add_namespace.sh
@@ -6,6 +6,7 @@ NAMESPACE_EXISTS=$(kubectl get namespace $NAMESPACE --ignore-not-found)
 if [[ -z "$NAMESPACE_EXISTS" ]]; then
     echo "Creating namespace $NAMESPACE"
     kubectl create namespace $NAMESPACE
+    echo "Namespace added!"
 else   
     echo "Namespace $NAMESPACE already present"
 fi 
