aae3724682e393d674b41fe729d23d75b4061f8c
João Afonso <joaoafonso386@gmail.com>
2024-02-25 23:30:00 +0000

    Add initial CI script with travis

--- kubernetes/scripts/ci/install.sh
+++ kubernetes/scripts/ci/install.sh
@@ -4,7 +4,9 @@ set -e
 for dir in */ *.yaml; do
 
     if [ "$(basename "$dir")" != "scripts" ]; then
-        kubectl apply -f $dir
+        kubectl apply -f $dir || exit 1 
     fi
 
 done
+
+echo "Kubernetes resources uploaded successfully!"
