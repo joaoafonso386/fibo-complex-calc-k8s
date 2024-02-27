0c599b86606f6eef4d27916e716567add6966a2b
João Afonso <joaoafonso386@gmail.com>
2024-02-27 01:10:58 +0000

    Update deploy script with unick container tags

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-docker build -t zigoto/fibo-client -f ./client/Dockerfile ./client
-docker build -t zigoto/fibo-server -f ./server/Dockerfile ./server
-docker build -t zigoto/fibo-worker -f ./worker/Dockerfile ./worker
+docker build -t zigoto/fibo-client:latest -t zigoto/fibo-client:$SHA -f ./client/Dockerfile ./client
+docker build -t zigoto/fibo-server:latest -t zigoto/fibo-server:$SHA -f ./server/Dockerfile ./server
+docker build -t zigoto/fibo-worker -t zigoto/fibo-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push zigoto/fibo-client
-docker push zigoto/fibo-server
-docker push zigoto/fibo-worker
+docker push zigoto/fibo-client:latest
+docker push zigoto/fibo-server:latest
+docker push zigoto/fibo-worker:latest
 
-sh ./kubernetes/scripts/ci/install.sh
+docker push zigoto/fibo-client:$SHA
+docker push zigoto/fibo-server:$SHA
+docker push zigoto/fibo-worker:$SHA
 
-kubectl set image deployment/server-deployment server=zigoto/fibo-server
+cd ./kubernetes
+
+sh ./scripts/ci/install.sh
+
+kubectl set image deployment/server-deployment server=zigoto/fibo-server:$SHA
+kubectl set image deployment/client-deployment client=zigoto/fibo-client:$SHA
+kubectl set image deployment/worker-deployment worker=zigoto/fibo-worker:$SHA
