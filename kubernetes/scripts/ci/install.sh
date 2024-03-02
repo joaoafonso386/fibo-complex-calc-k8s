974477455ef0f7063feab11b41e8a32e75ca4e72
João Afonso <joaoafonso386@gmail.com>
2024-03-02 00:17:17 +0000

    Skip ingress deployment if already exists to avoid error

--- kubernetes/scripts/ci/install.sh
+++ kubernetes/scripts/ci/install.sh
@@ -3,6 +3,13 @@ set -e
 
 INGRESS_EXISTS=$(kubectl get -f ingress.yaml -n ingress-nginx --ignore-not-found)
 
+if [ -z "$INGRESS_EXISTS" ]; then
+    echo "Ingress resource already exists. Skipping..."
+else
+    echo "Ingress resource doesn't exist. Applying..."
+    kubectl apply -f ingress.yaml -n ingress-nginx || exit 1
+fi
+
 for dir in */; do
 
     if [ "$(basename "$dir")" != "scripts" ]; then
